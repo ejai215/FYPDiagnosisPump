0601e2155202cda5499e155135881997450d85ba
ejai215 <121840136+ejai215@users.noreply.github.com>
2023-01-18 21:13:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def predict():
     model = pickle.load(open("PumpPredictionModel.pkl", "rb"))
     
     
-    data = [[inputQuery1, inputQuery2, inputQuery3, inputQuery4, inputQuery5, inputQuery6, inputQuery7, inputQuery8, inputQuery9, inputQuery]]
+    data = [[inputQuery1, inputQuery2, inputQuery3, inputQuery4, inputQuery5, inputQuery6, inputQuery7, inputQuery8, inputQuery9, inputQuery10]]
     new_df = pd.DataFrame(data, columns = ['mean', 'max', 'kurtosis', 'variance','onenorm','mean1', 'max1', 'kurtosis1','variance1','onenorm1'])
     
     single = model.predict(new_df)
