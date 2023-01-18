8be5f0ff748db0d3b07a6f5a172ae4a3d6389173
ejai215 <121840136+ejai215@users.noreply.github.com>
2023-01-18 20:50:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -34,24 +34,35 @@ def predict():
     inputQuery3 = request.form['query3']
     inputQuery4 = request.form['query4']
     inputQuery5 = request.form['query5']
+    inputQuery6 = request.form['query6']
+    inputQuery7 = request.form['query7']
+    inputQuery8 = request.form['query8']
+    inputQuery9 = request.form['query9']
+    inputQuery10 = request.form['query10']
 
-    model = pickle.load(open("breastCancerModel.pkl", "rb"))
+    model = pickle.load(open("PumpPredictionModel.pkl", "rb"))
     
     
     data = [[inputQuery1, inputQuery2, inputQuery3, inputQuery4, inputQuery5]]
-    new_df = pd.DataFrame(data, columns = ['texture_mean', 'perimeter_mean', 'smoothness_mean', 'compactness_mean', 'symmetry_mean'])
+    new_df = pd.DataFrame(data, columns = ['mean', 'max', 'kurtosis', 'variance','onenorm','mean1', 'max1', 'kurtosis1','variance1','onenorm1'])
     
     single = model.predict(new_df)
-    probablity = model.predict_proba(new_df)[:,1]
+    probablity = model.predict_proba(new_df)[:,0]
     
-    if single==1:
-        o1 = "The patient is diagnosed with Breast Cancer"
+    if single==0:
+        o1 = "The pump status is health"
+        o2 = "Confidence: {}".format(probablity*100)
+    elif single==1:
+        o1 = "The pump status is severe"
+        o2 = "Confidence: {}".format(probablity*100)
+    elif single==2:
+        o1 = "The pump status is mild"
         o2 = "Confidence: {}".format(probablity*100)
     else:
-        o1 = "The patient is not diagnosed with Breast Cancer"
+        o1 = "The pump status is mild2"
         o2 = "Confidence: {}".format(probablity*100)
         
-    return render_template('home.html', output1=o1, output2=o2, query1 = request.form['query1'], query2 = request.form['query2'],query3 = request.form['query3'],query4 = request.form['query4'],query5 = request.form['query5'])
+    return render_template('home.html', output1=o1, output2=o2, query1 = request.form['query1'], query2 = request.form['query2'],query3 = request.form['query3'],query4 = request.form['query4'],query5 = request.form['query5'],query6 = request.form['query6'],query7 = request.form['query7'],query8 = request.form['query8'],query9 = request.form['query9'],query10 = request.form['query10'])
     
 if __name__ == "__main__":
     app.run()
